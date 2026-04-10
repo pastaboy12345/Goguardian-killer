dfd332b12e01e8b4f149a10ef1f29e48fbf85820
pastaboy12345 <omgdddd096@gmail.com>
2026-04-09 18:55:32 -0700

    Update bookmarklet to fetch from new script URL

--- TheActualBookmarklet.js
+++ TheActualBookmarklet.js
@@ -1,1 +1,1 @@
-javascript:fetch(`https://raw.githubusercontent.com/pastaboy12345/Goguardian-killer/main/Exploit_Not_The_Bookmarklet-fixed.js`).then(data=>{data.text().then(text=>{eval(text)})});
+javascript:fetch(`https://raw.githubusercontent.com/pastaboy12345/Goguardian-killer/main/Exploit_Not_The_Bookmarklet.js`).then(data=>{data.text().then(text=>{eval(text)})});
